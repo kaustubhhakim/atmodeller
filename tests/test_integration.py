a60ab81418fab0f32689e0216c3ed0b85b228a60
Dan J Bower <djbower@users.noreply.github.com>
2023-06-05 11:42:47 +0200

    Integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,13 +1,388 @@
 """Integration tests.
 
-Tests that the package reports reasonable values for certain interior-atmosphere systems. These are
-quite rudimentary tests, but at least confirm that nothing fundamental is broken with the code.
+Tests to ensure that 'correct' values are returned for certain interior-atmosphere systems. 
+These are quite rudimentary tests, but at least confirm that nothing fundamental is broken with the
+code.
 
 """
 
-from atmodeller import __version__
+import numpy as np
+
+from atmodeller import (
+    OCEAN_MOLES,
+    InteriorAtmosphereSystem,
+    MolarMasses,
+    Molecule,
+    Planet,
+    SystemConstraint,
+    __version__,
+)
+from atmodeller.thermodynamics import (
+    BasaltDixonCO2,
+    LibourelN2,
+    NoSolubility,
+    PeridotiteH2O,
+)
+
+# Tolerances to compare the test results with predefined 'correct' output.
+rtol: float = 1.0e-5
+atol: float = 1.0e-8
 
 
 def test_version():
     """Test version."""
     assert __version__ == "0.1.0"
+
+
+# region oxygen fugacity
+
+
+def test_hydrogen_species_oxygen_fugacity_buffer() -> None:
+    """Tests H2-H2O at the IW buffer."""
+
+    molecules: list[Molecule] = [
+        Molecule(name="H2O", solubility=PeridotiteH2O()),
+        Molecule(name="H2", solubility=NoSolubility()),
+        Molecule(name="O2", solubility=NoSolubility()),
+    ]
+
+    oceans: float = 1
+    planet: Planet = Planet()
+    molar_masses: MolarMasses = MolarMasses()
+    h_kg: float = oceans * OCEAN_MOLES * molar_masses.H2
+
+    constraints: list[SystemConstraint] = [
+        SystemConstraint(species="H", value=h_kg, field="mass"),
+    ]
+
+    system: InteriorAtmosphereSystem = InteriorAtmosphereSystem(
+        molecules=molecules, planet=planet
+    )
+
+    planet.fo2_shift = 0
+    target_pressures: np.ndarray = np.array(
+        [5.48791203e-01, 3.90999460e-08, 3.74396042e-01]
+    )
+    system.solve(constraints, fo2_constraint=True)
+    assert np.isclose(target_pressures, system.pressures, rtol=rtol, atol=atol).all()
+
+
+def test_hydrogen_species_oxygen_fugacity_buffer_shift_positive() -> None:
+    """Tests H2-H2O at the IW buffer+2."""
+
+    molecules: list[Molecule] = [
+        Molecule(name="H2O", solubility=PeridotiteH2O()),
+        Molecule(name="H2", solubility=NoSolubility()),
+        Molecule(name="O2", solubility=NoSolubility()),
+    ]
+
+    oceans: float = 1
+    planet: Planet = Planet()
+    molar_masses: MolarMasses = MolarMasses()
+    h_kg: float = oceans * OCEAN_MOLES * molar_masses.H2
+
+    constraints: list[SystemConstraint] = [
+        SystemConstraint(species="H", value=h_kg, field="mass"),
+    ]
+
+    system: InteriorAtmosphereSystem = InteriorAtmosphereSystem(
+        molecules=molecules, planet=planet
+    )
+
+    planet.fo2_shift = 2
+    target_pressures: np.ndarray = np.array(
+        [5.54886307e-02, 3.90999460e-06, 3.78554241e-01]
+    )
+    system.solve(constraints, fo2_constraint=True)
+    assert np.isclose(target_pressures, system.pressures, rtol=rtol, atol=atol).all()
+
+
+def test_hydrogen_species_oxygen_fugacity_buffer_shift_negative() -> None:
+    """Tests H2-H2O at the IW buffer-2."""
+
+    molecules: list[Molecule] = [
+        Molecule(name="H2O", solubility=PeridotiteH2O()),
+        Molecule(name="H2", solubility=NoSolubility()),
+        Molecule(name="O2", solubility=NoSolubility()),
+    ]
+
+    oceans: float = 1
+    planet: Planet = Planet()
+    molar_masses: MolarMasses = MolarMasses()
+    h_kg: float = oceans * OCEAN_MOLES * molar_masses.H2
+
+    constraints: list[SystemConstraint] = [
+        SystemConstraint(species="H", value=h_kg, field="mass"),
+    ]
+
+    system: InteriorAtmosphereSystem = InteriorAtmosphereSystem(
+        molecules=molecules, planet=planet
+    )
+
+    planet.fo2_shift = -2
+    target_pressures: np.ndarray = np.array(
+        [4.45324659e00, 3.90999460e-10, 3.03809152e-01]
+    )
+    system.solve(constraints, fo2_constraint=True)
+    assert np.isclose(target_pressures, system.pressures, rtol=rtol, atol=atol).all()
+
+
+# endregion
+
+# region number of oceans
+
+
+def test_hydrogen_species_five_oceans() -> None:
+    """Tests H2-H2O for five H oceans."""
+
+    molecules: list[Molecule] = [
+        Molecule(name="H2O", solubility=PeridotiteH2O()),
+        Molecule(name="H2", solubility=NoSolubility()),
+        Molecule(name="O2", solubility=NoSolubility()),
+    ]
+
+    oceans: float = 5
+    planet: Planet = Planet()
+    molar_masses: MolarMasses = MolarMasses()
+    h_kg: float = oceans * OCEAN_MOLES * molar_masses.H2
+
+    constraints: list[SystemConstraint] = [
+        SystemConstraint(species="H", value=h_kg, field="mass"),
+    ]
+
+    system: InteriorAtmosphereSystem = InteriorAtmosphereSystem(
+        molecules=molecules, planet=planet
+    )
+
+    planet.fo2_shift = 0
+    target_pressures: np.ndarray = np.array(
+        [1.29744174e01, 3.90999460e-08, 8.85140016e00]
+    )
+    system.solve(constraints, fo2_constraint=True)
+
+    assert np.isclose(target_pressures, system.pressures, rtol=rtol, atol=atol).all()
+
+
+def test_hydrogen_species_ten_oceans() -> None:
+    """Tests H2-H2O for ten H oceans."""
+
+    molecules: list[Molecule] = [
+        Molecule(name="H2O", solubility=PeridotiteH2O()),
+        Molecule(name="H2", solubility=NoSolubility()),
+        Molecule(name="O2", solubility=NoSolubility()),
+    ]
+
+    oceans: float = 10
+    planet: Planet = Planet()
+    molar_masses: MolarMasses = MolarMasses()
+    h_kg: float = oceans * OCEAN_MOLES * molar_masses.H2
+
+    constraints: list[SystemConstraint] = [
+        SystemConstraint(species="H", value=h_kg, field="mass"),
+    ]
+
+    system: InteriorAtmosphereSystem = InteriorAtmosphereSystem(
+        molecules=molecules, planet=planet
+    )
+
+    planet.fo2_shift = 0
+    target_pressures: np.ndarray = np.array(
+        [4.86826757e01, 3.90999460e-08, 3.32122691e01]
+    )
+    system.solve(constraints, fo2_constraint=True)
+    assert np.isclose(target_pressures, system.pressures, rtol=rtol, atol=atol).all()
+
+
+# endregion
+
+# region temperature
+
+
+def test_hydrogen_species_temperature() -> None:
+    """Tests H2-H2O at a different temperature."""
+
+    molecules: list[Molecule] = [
+        Molecule(name="H2O", solubility=PeridotiteH2O()),
+        Molecule(name="H2", solubility=NoSolubility()),
+        Molecule(name="O2", solubility=NoSolubility()),
+    ]
+
+    oceans: float = 1
+    planet: Planet = Planet()
+    molar_masses: MolarMasses = MolarMasses()
+    h_kg: float = oceans * OCEAN_MOLES * molar_masses.H2
+
+    constraints: list[SystemConstraint] = [
+        SystemConstraint(species="H", value=h_kg, field="mass"),
+    ]
+
+    system: InteriorAtmosphereSystem = InteriorAtmosphereSystem(
+        molecules=molecules, planet=planet
+    )
+
+    planet.surface_temperature = 1500.0  # K
+    target_pressures: np.ndarray = np.array(
+        [3.52680793e-01, 3.94851706e-12, 3.76314774e-01]
+    )
+    system.solve(constraints, fo2_constraint=True)
+    assert np.isclose(target_pressures, system.pressures, rtol=rtol, atol=atol).all()
+
+
+# endregion
+
+# region C over H ratio
+
+
+def test_hydrogen_and_carbon_species() -> None:
+    """Tests H2-H2O and CO-CO2."""
+
+    molecules: list[Molecule] = [
+        Molecule(name="H2O", solubility=PeridotiteH2O()),
+        Molecule(name="H2", solubility=NoSolubility()),
+        Molecule(name="O2", solubility=NoSolubility()),
+        Molecule(name="CO", solubility=NoSolubility()),
+        Molecule(name="CO2", solubility=BasaltDixonCO2()),
+    ]
+
+    oceans: float = 1
+    ch_ratio: float = 1
+    planet: Planet = Planet()
+    molar_masses: MolarMasses = MolarMasses()
+    h_kg: float = oceans * OCEAN_MOLES * molar_masses.H2
+    c_kg: float = ch_ratio * h_kg
+
+    constraints: list[SystemConstraint] = [
+        SystemConstraint(species="H", value=h_kg, field="mass"),
+        SystemConstraint(species="C", value=c_kg, field="mass"),
+    ]
+
+    system: InteriorAtmosphereSystem = InteriorAtmosphereSystem(
+        molecules=molecules, planet=planet
+    )
+
+    target_pressures: np.ndarray = np.array(
+        [6.23020615e01, 5.54579782e-01, 3.90999460e-08, 9.46924812e00, 3.78345124e-01]
+    )
+    system.solve(constraints, fo2_constraint=True)
+    assert np.isclose(target_pressures, system.pressures, rtol=rtol, atol=atol).all()
+
+
+def test_hydrogen_and_carbon_species_five_ch_ratio() -> None:
+    """Tests H2-H2O and CO-CO2 for C/H=5."""
+
+    molecules: list[Molecule] = [
+        Molecule(name="H2O", solubility=PeridotiteH2O()),
+        Molecule(name="H2", solubility=NoSolubility()),
+        Molecule(name="O2", solubility=NoSolubility()),
+        Molecule(name="CO", solubility=NoSolubility()),
+        Molecule(name="CO2", solubility=BasaltDixonCO2()),
+    ]
+
+    oceans: float = 1
+    ch_ratio: float = 5
+    planet: Planet = Planet()
+    molar_masses: MolarMasses = MolarMasses()
+    h_kg: float = oceans * OCEAN_MOLES * molar_masses.H2
+    c_kg: float = ch_ratio * h_kg
+
+    constraints: list[SystemConstraint] = [
+        SystemConstraint(species="H", value=h_kg, field="mass"),
+        SystemConstraint(species="C", value=c_kg, field="mass"),
+    ]
+
+    system: InteriorAtmosphereSystem = InteriorAtmosphereSystem(
+        molecules=molecules, planet=planet
+    )
+
+    target_pressures: np.ndarray = np.array(
+        [3.13803353e02, 5.54597021e-01, 3.90999460e-08, 4.76947590e01, 3.78356885e-01]
+    )
+    system.solve(constraints, fo2_constraint=True)
+    assert np.isclose(target_pressures, system.pressures, rtol=rtol, atol=atol).all()
+
+
+def test_hydrogen_and_carbon_species_ten_ch_ratio() -> None:
+    """Tests H2-H2O and CO-CO2 for C/H=10."""
+
+    molecules: list[Molecule] = [
+        Molecule(name="H2O", solubility=PeridotiteH2O()),
+        Molecule(name="H2", solubility=NoSolubility()),
+        Molecule(name="O2", solubility=NoSolubility()),
+        Molecule(name="CO", solubility=NoSolubility()),
+        Molecule(name="CO2", solubility=BasaltDixonCO2()),
+    ]
+
+    oceans: float = 1
+    ch_ratio: float = 10
+    planet: Planet = Planet()
+    molar_masses: MolarMasses = MolarMasses()
+    h_kg: float = oceans * OCEAN_MOLES * molar_masses.H2
+    c_kg: float = ch_ratio * h_kg
+
+    constraints: list[SystemConstraint] = [
+        SystemConstraint(species="H", value=h_kg, field="mass"),
+        SystemConstraint(species="C", value=c_kg, field="mass"),
+    ]
+
+    system: InteriorAtmosphereSystem = InteriorAtmosphereSystem(
+        molecules=molecules, planet=planet
+    )
+
+    target_pressures: np.ndarray = np.array(
+        [6.28301248e02, 5.54599166e-01, 3.90999460e-08, 9.54950810e01, 3.78358348e-01]
+    )
+    system.solve(constraints, fo2_constraint=True)
+    assert np.isclose(target_pressures, system.pressures, rtol=rtol, atol=atol).all()
+
+
+# endregion
+
+# region nitrogen
+
+
+def test_hydrogen_and_carbon_species_with_nitrogen() -> None:
+    """Tests H2-H2O and CO-CO2 and N."""
+
+    molecules: list[Molecule] = [
+        Molecule(name="H2O", solubility=PeridotiteH2O()),
+        Molecule(name="H2", solubility=NoSolubility()),
+        Molecule(name="O2", solubility=NoSolubility()),
+        Molecule(name="CO", solubility=NoSolubility()),
+        Molecule(name="CO2", solubility=BasaltDixonCO2()),
+        Molecule(name="N2", solubility=LibourelN2()),
+    ]
+
+    oceans: float = 1
+    ch_ratio: float = 1
+    nitrogen_ppmw: float = 2.8
+    planet: Planet = Planet()
+    molar_masses: MolarMasses = MolarMasses()
+    h_kg: float = oceans * OCEAN_MOLES * molar_masses.H2
+    c_kg: float = ch_ratio * h_kg
+    n_kg: float = nitrogen_ppmw * 1.0e-6 * planet.mantle_mass
+
+    constraints: list[SystemConstraint] = [
+        SystemConstraint(species="H", value=h_kg, field="mass"),
+        SystemConstraint(species="C", value=c_kg, field="mass"),
+        SystemConstraint(species="N", value=n_kg, field="mass"),
+    ]
+
+    system: InteriorAtmosphereSystem = InteriorAtmosphereSystem(
+        molecules=molecules, planet=planet
+    )
+
+    target_pressures: np.ndarray = np.array(
+        [
+            6.21878608e01,
+            5.54575349e-01,
+            2.29155306e00,
+            3.90999460e-08,
+            9.45189083e00,
+            3.78342099e-01,
+        ]
+    )
+    system.solve(constraints, fo2_constraint=True)
+    assert np.isclose(target_pressures, system.pressures, rtol=rtol, atol=atol).all()
+
+
+# endregion
