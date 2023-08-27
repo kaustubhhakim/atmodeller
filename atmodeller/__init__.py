3a8c3ee9378eef3bae19778872513e6c58379dcd
Dan J Bower <djbower@users.noreply.github.com>
2023-08-27 10:56:01 +0200

    Option to write the logger DEBUG output to a file

--- atmodeller/__init__.py
+++ atmodeller/__init__.py
@@ -14,25 +14,56 @@ GAS_CONSTANT: float = constants.gas_constant  # J/K/mol.
 GRAVITATIONAL_CONSTANT: float = constants.gravitational_constant  # m^3/kg/s^2.
 OCEAN_MOLES: float = 7.68894973907177e22  # Moles of H2 (or H2O) in one present-day Earth ocean.
 
-DATA_ROOT_PATH = importlib.resources.files("atmodeller.data")
+DATA_ROOT_PATH = importlib.resources.files("%s.data" % __package__)
 
 # Create the package logger.
+# https://docs.python.org/3/howto/logging.html#library-config
 logger: logging.Logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 
-def debug_logger() -> None:
-    """Setup the logging for debugging."""
-    logger: logging.Logger = logging.getLogger(__name__)
-    logger.handlers = []
-    handler: logging.Handler = logging.StreamHandler()
-    logger.setLevel(logging.DEBUG)
-    # Complex formatter.
-    # fmt: str = "[%(asctime)s - %(name)-20s - %(lineno)03d - %(levelname)-9s -
-    # %(funcName)s()] %(message)s"
-    # datefmt: str = "Y-%m-%d %H:%M:%S"
-    fmt: str = "%(asctime)s - %(name)-30s - %(levelname)-9s - %(message)s"
+def complex_formatter() -> logging.Formatter:
+    """Complex formatter."""
+    fmt: str = "[%(asctime)s - %(name)-30s - %(lineno)03d - %(levelname)-9s - %(funcName)s()] - %(message)s"
+    datefmt: str = "%Y-%m-%d %H:%M:%S"
+    formatter: logging.Formatter = logging.Formatter(fmt, datefmt=datefmt)
+    return formatter
+
+
+def simple_formatter() -> logging.Formatter:
+    """Simple formatter."""
+    fmt: str = "[%(asctime)s - %(name)-30s - %(levelname)-9s] - %(message)s"
     datefmt: str = "%H:%M:%S"
     formatter: logging.Formatter = logging.Formatter(fmt, datefmt=datefmt)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    return formatter
+
+
+def debug_logger() -> None:
+    """Setup the logging for debugging: DEBUG to the console."""
+    # Console logger.
+    logger: logging.Logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logger.handlers = []
+    console_handler: logging.Handler = logging.StreamHandler()
+    console_formatter: logging.Formatter = simple_formatter()
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
+
+
+def debug_file_logger() -> None:
+    """Setup the logging to a file (DEBUG) and to the console (INFO)."""
+    # Console logger.
+    logger: logging.Logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logger.handlers = []
+    console_handler: logging.Handler = logging.StreamHandler()
+    console_formatter: logging.Formatter = simple_formatter()
+    console_handler.setFormatter(console_formatter)
+    console_handler.setLevel(logging.INFO)
+    logger.addHandler(console_handler)
+    # File logger.
+    file_handler: logging.Handler = logging.FileHandler("%s.log" % __package__)
+    file_formatter: logging.Formatter = complex_formatter()
+    file_handler.setFormatter(file_formatter)
+    file_handler.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
