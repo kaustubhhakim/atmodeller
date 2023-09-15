99eebabc151381464b216e3a55942d09d128b96c
Dan J Bower <djbower@users.noreply.github.com>
2023-09-15 17:28:09 +0200

    Testing Shi and Saxena model

--- atmodeller/eos.py
+++ atmodeller/eos.py
@@ -19,6 +19,7 @@ import logging
 from dataclasses import dataclass, field
 
 import numpy as np
+from scipy.constants import kilo
 
 from atmodeller.eos_interfaces import (
     MRKABC,
@@ -26,6 +27,9 @@ from atmodeller.eos_interfaces import (
     FugacityModelABC,
     MRKExplicitABC,
     MRKImplicitABC,
+    ShiSaxenaABC,
+    ShiSaxenaHighPressure,
+    ShiSaxenaLowPressure,
     VirialCompensation,
 )
 
@@ -459,12 +463,17 @@ class CorkCO(CorkCorrespondingStates):
 
 @dataclass(kw_only=True)
 class CorkS2(CorkCorrespondingStates):
-    """Critical constants for S2. See base class."""
+    """Critical constants for S2. See base class.
 
-    # Data not in The Properties of Gases and Liquids.  Use S instead?
+    Shi and Saxena, Thermodynamic modeling of the C-H-O-S fluid system, American Mineralogist,
+    Volume 77, pages 1038-1049, 1992. See table 2, critical data of C-H-O-S fluid phases.
 
-    Tc: float  # TODO = field(init=False, default=132.9)
-    Pc: float  # TODO = field(init=False, default=0.0350)
+    http://www.minsocam.org/ammin/AM77/AM77_1038.pdf
+
+    """
+
+    Tc: float = field(init=False, default=208.15)
+    Pc: float = field(init=False, default=0.072954)
 
 
 @dataclass(kw_only=True)
@@ -478,185 +487,71 @@ class CorkH2S(CorkCorrespondingStates):
     Pc: float = field(init=False, default=0.08963)
 
 
-# FIXME: This class likely has a bug. Need to compare this model against others or dig in the paper
-# to confirm unit conversions etc.
 @dataclass(kw_only=True)
-class SaxenaFeiH2(FugacityModelABC):
-    """Saxena and Fei (1988) fugacity model for H2.
-
-    https://www.sciencedirect.com/science/article/abs/pii/0016703788902736
-
-    Attributes:
-        Tc: Critical temperature in kelvin.
-        Pc: Critical pressure in bar.
-        a_coefficients: a coefficients (see paper).
-        b_coefficients: a coefficients (see paper).
-        c_coefficients: a coefficients (see paper).
-        d_coefficients: a coefficients (see paper).
-        scaling: Scaling is unity for bar.
-        GAS_CONSTANT: Gas constant with the appropriate units.
-    """
-
-    Tc: float = field(init=False, default=33.1)  # kelvin
-    Pc: float = field(init=False, default=13)  # bar
-
-    a_coefficients: tuple[float, float, float, float] = field(
-        init=False, default=(1.6688, -2.0759, -9.6173, -0.1694)
+class ShiSaxenaHighPressureH2(ShiSaxenaHighPressure):
+    Tc: float = field(init=False, default=33.25)
+    Pc: float = field(init=False, default=12.9696)
+    a_coefficients: tuple[float, ...] = field(
+        init=False, default=(2.2615, 0, -6.8712e1, 0, -1.0573e4, 0, 0, -1.6936e-1)
     )
-    b_coefficients: tuple[float, float, float, float] = field(
-        init=False, default=(-2.0410e-3, 7.9230e-2, 5.4295e-2, 4.0887e-4)
+    b_coefficients: tuple[float, ...] = field(
+        init=False, default=(-2.6707e-4, 0, 2.0173e-1, 0, 4.5759, 0, 0, 3.1452e-5)
     )
-    c_coefficients: tuple[float, float, float, float] = field(
-        init=False, default=(-2.1693e-7, 1.7406e-6, -2.1885e-4, 5.0897e-5)
+    c_coefficients: tuple[float, ...] = field(
+        init=False, default=(-2.3376e-9, 0, 3.4091e-7, 0, -1.4188e-3, 0, 0, 3.0117e-10)
     )
-    d_coefficients: tuple[float, float, float, float] = field(
-        init=False, default=(-7.1635e-12, 1.6197e-10, -4.8181e-9, 0)
+    d_coefficients: tuple[float, ...] = field(
+        init=False, default=(-3.2606e-15, 0, 2.4402e-12, 0, -2.4027e-9, 0, 0, 0)
     )
 
-    def _get_compressibility_coefficient(
-        self, temperature: float, coefficients: tuple[float, float, float, float]
-    ) -> float:
-        """General form of the coefficients for the compressibility calculation.
+
+@dataclass(kw_only=True)
+class ShiSaxenaLowPressureH2(ShiSaxenaLowPressure):
+    Tc: float = field(init=False, default=33.25)
+    Pc: float = field(init=False, default=12.9696)
+    a_coefficients: tuple[float, ...] = field(init=False, default=(1, 0, 0, 0, 0, 0))
+    b_coefficients: tuple[float, ...] = field(init=False, default=(0, 0.9827e-1, 0, -0.2709, 0))
+    c_coefficients: tuple[float, ...] = field(init=False, default=(0, 0, -0.1030e-2, 0, 0.1427e-1))
+    d_coefficients: tuple[float, ...] = field(init=False, default=(0, 0, 0, 0, 0))
+
+
+@dataclass(kw_only=True)
+class ShiSaxenaH2(FugacityModelABC):
+    low_pressure_eos: ShiSaxenaABC = field(default_factory=ShiSaxenaLowPressureH2)
+    high_pressure_eos: ShiSaxenaABC = field(default_factory=ShiSaxenaHighPressureH2)
+
+    def get_value(self, *, temperature: float, pressure: float) -> float:
+        """Evaluates the fugacity coefficient at temperature and pressure.
+
+        Note that the input 'pressure' must ALWAYS be in bar, so it is scaled here using
+        'self.scaling' since self.fugacity_coefficient requires the internal units of pressure.
 
         Args:
             temperature: Temperature in kelvin.
-            coefficients: Tuple of the coefficients a, b, c, or d.
-
-        Returns
-            The relevant coefficient.
-        """
-        Tr: float = self.reduced_temperature(temperature)
-        coefficient: float = (
-            coefficients[0]
-            + coefficients[1] / Tr
-            + coefficients[2] / Tr**2
-            + coefficients[3] * np.log(Tr)
-        )
-
-        return coefficient
-
-    def a(self, temperature: float) -> float:
-        """a parameter.
-
-        Args:
-            Temperature: temperature in kelvin.
+            pressure: Pressure in bar.
 
         Returns:
-            a parameter.
+            Fugacity coefficient evaluated at temperature and pressure, which is dimensionaless.
         """
-        return self._get_compressibility_coefficient(temperature, self.a_coefficients)
+        pressure /= self.scaling
 
-    def b(self, temperature: float) -> float:
-        """b parameter.
-
-        Args:
-            Temperature: temperature in kelvin.
-
-        Returns:
-            b parameter.
-        """
-        return self._get_compressibility_coefficient(temperature, self.b_coefficients)
-
-    def c(self, temperature: float) -> float:
-        """c parameter.
-
-        Args:
-            Temperature: temperature in kelvin.
-
-        Returns:
-            c parameter.
-        """
-        return self._get_compressibility_coefficient(temperature, self.c_coefficients)
-
-    def d(self, temperature: float) -> float:
-        """d parameter.
-
-        Args:
-            Temperature: temperature in kelvin.
-
-        Returns:
-            d parameter.
-        """
-        return self._get_compressibility_coefficient(temperature, self.d_coefficients)
-
-    def compressibility_parameter(self, temperature: float, pressure: float) -> float:
-        """Compressibility parameter at temperature and pressure.
-
-        Args:
-            temperature: Temperature in kelvin.
-            pressure: Pressure in kbar.
-
-        Returns:
-            The compressibility parameter, Z.
-        """
-
-        Pr: float = self.reduced_pressure(pressure)
-        Z: float = (
-            self.a(temperature)
-            + self.b(temperature) * Pr
-            + self.c(temperature) * Pr**2
-            + self.d(temperature) * Pr**3
-        )
-
-        return Z
-
-    def reduced_pressure(self, pressure: float) -> float:
-        """Reduced pressure.
-
-        Args:
-            pressure: Pressure in kbar.
-
-        Returns:
-            The reduced pressure, which is dimensionless.
-        """
-        return pressure / self.Pc
-
-    def reduced_temperature(self, temperature: float) -> float:
-        """Reduced temperature.
-
-        Args:
-            temperature: Temperature in kelvin.
-
-        Returns:
-            The reduced temperature, which is dimensionless.
-        """
-        return temperature / self.Tc
+        if pressure >= 1e3:
+            return self.high_pressure_eos.get_value(temperature=temperature, pressure=pressure)
+        else:
+            return self.low_pressure_eos.get_value(temperature=temperature, pressure=pressure)
 
     def volume(self, temperature: float, pressure: float) -> float:
-        """Volume.
-
-        Args:
-            temperature: Temperature in kelvin.
-            pressure: Pressure.
-
-        Returns:
-            Volume.
-        """
-        Z: float = self.compressibility_parameter(temperature, pressure)
-        volume: float = Z * self.GAS_CONSTANT * temperature / pressure
-
-        return volume
+        if pressure / self.scaling >= 1e3:
+            return self.high_pressure_eos.volume(temperature=temperature, pressure=pressure)
+        else:
+            return self.low_pressure_eos.volume(temperature=temperature, pressure=pressure)
 
     def volume_integral(self, temperature: float, pressure: float) -> float:
-        """Volume integral (V dP).
-
-        Args:
-            temperature: Temperature in kelvin.
-            pressure: Pressure.
-
-        Returns:
-            Volume integral.
-        """
-        Pr: float = self.reduced_pressure(pressure)
-        volume_integral: float = (
-            (
-                self.a(temperature) * np.log(pressure)
-                + self.b(temperature) * Pr
-                + 0.5 * self.c(temperature) * Pr**2
-                + (1 / 3) * self.d(temperature) * Pr**3
+        if pressure / self.scaling >= 1e3:
+            return self.high_pressure_eos.volume_integral(
+                temperature=temperature, pressure=pressure
             )
-            * self.GAS_CONSTANT
-            * temperature
-        )
-
-        return volume_integral
+        else:
+            return self.low_pressure_eos.volume_integral(
+                temperature=temperature, pressure=pressure
+            )
--- atmodeller/eos_interfaces.py
+++ atmodeller/eos_interfaces.py
@@ -763,3 +763,247 @@ class CorkFullABC(MRKImplicitABC):
             volume_integral += self.virial.volume_integral(temperature, pressure)
 
         return volume_integral
+
+
+@dataclass(kw_only=True)
+class ShiSaxenaABC(FugacityModelABC):
+    """Shi and Saxena fugacity model.
+
+    Shi and Saxena, Thermodynamic modeling of the C-H-O-S fluid system, American Mineralogist,
+    Volume 77, pages 1038-1049, 1992. See table 2, critical data of C-H-O-S fluid phases.
+
+    http://www.minsocam.org/ammin/AM77/AM77_1038.pdf
+
+    Attributes:
+        Tc: Critical temperature in kelvin.
+        Pc: Critical pressure in bar.
+        a_coefficients: a coefficients (see paper).
+        b_coefficients: b coefficients (see paper).
+        c_coefficients: c coefficients (see paper).
+        d_coefficients: d coefficients (see paper).
+        scaling: Scaling is unity for bar.
+        GAS_CONSTANT: Gas constant with the appropriate units.
+    """
+
+    Tc: float
+    Pc: float
+    # TODO: Different P0 to P0 used in Holland and Powell
+    # Integration start for pressure.
+    P0: float = field(init=False, default=1)  # 1 bar
+    a_coefficients: tuple[float, ...]
+    b_coefficients: tuple[float, ...]
+    c_coefficients: tuple[float, ...]
+    d_coefficients: tuple[float, ...]
+
+    @abstractmethod
+    def _get_compressibility_coefficient(
+        self, temperature: float, coefficients: tuple[float, ...]
+    ) -> float:
+        """General form of the coefficients for the compressibility calculation.
+
+        Args:
+            temperature: Temperature in kelvin.
+            coefficients: Tuple of the coefficients a, b, c, or d.
+
+        Returns
+            The relevant coefficient.
+        """
+        ...
+
+    def a(self, temperature: float) -> float:
+        """a parameter.
+
+        Args:
+            Temperature: temperature in kelvin.
+
+        Returns:
+            a parameter.
+        """
+        return self._get_compressibility_coefficient(temperature, self.a_coefficients)
+
+    def b(self, temperature: float) -> float:
+        """b parameter.
+
+        Args:
+            Temperature: temperature in kelvin.
+
+        Returns:
+            b parameter.
+        """
+        return self._get_compressibility_coefficient(temperature, self.b_coefficients)
+
+    def c(self, temperature: float) -> float:
+        """c parameter.
+
+        Args:
+            Temperature: temperature in kelvin.
+
+        Returns:
+            c parameter.
+        """
+        return self._get_compressibility_coefficient(temperature, self.c_coefficients)
+
+    def d(self, temperature: float) -> float:
+        """d parameter.
+
+        Args:
+            Temperature: temperature in kelvin.
+
+        Returns:
+            d parameter.
+        """
+        return self._get_compressibility_coefficient(temperature, self.d_coefficients)
+
+    def compressibility_parameter(self, temperature: float, pressure: float) -> float:
+        """Compressibility parameter at temperature and pressure.
+
+        Args:
+            temperature: Temperature in kelvin.
+            pressure: Pressure in kbar.
+
+        Returns:
+            The compressibility parameter, Z.
+        """
+
+        Pr: float = self.reduced_pressure(pressure)
+        Z: float = (
+            self.a(temperature)
+            + self.b(temperature) * Pr
+            + self.c(temperature) * Pr**2
+            + self.d(temperature) * Pr**3
+        )
+
+        return Z
+
+    def reduced_pressure(self, pressure: float) -> float:
+        """Reduced pressure.
+
+        Args:
+            pressure: Pressure in kbar.
+
+        Returns:
+            The reduced pressure, which is dimensionless.
+        """
+        return pressure / self.Pc
+
+    @property
+    def reduced_pressure0(self) -> float:
+        """Reduced pressure.
+
+        Args:
+            pressure: Pressure in kbar.
+
+        Returns:
+            The reduced pressure, which is dimensionless.
+        """
+        return self.P0 / self.Pc
+
+    def reduced_temperature(self, temperature: float) -> float:
+        """Reduced temperature.
+
+        Args:
+            temperature: Temperature in kelvin.
+
+        Returns:
+            The reduced temperature, which is dimensionless.
+        """
+        return temperature / self.Tc
+
+    def volume(self, temperature: float, pressure: float) -> float:
+        """Volume.
+
+        Args:
+            temperature: Temperature in kelvin.
+            pressure: Pressure.
+
+        Returns:
+            Volume.
+        """
+        Z: float = self.compressibility_parameter(temperature, pressure)
+        volume: float = Z * self.GAS_CONSTANT * temperature / pressure
+
+        return volume
+
+    def volume_integral(self, temperature: float, pressure: float) -> float:
+        """Volume integral (V dP).
+
+        Args:
+            temperature: Temperature in kelvin.
+            pressure: Pressure.
+
+        Returns:
+            Volume integral.
+        """
+        Pr: float = self.reduced_pressure(pressure)
+        P0r: float = self.reduced_pressure0
+        volume_integral: float = (
+            (
+                self.a(temperature) * np.log(Pr / P0r)
+                + self.b(temperature) * (Pr - P0r)
+                + (1.0 / 2) * self.c(temperature) * (Pr**2 - P0r**2)
+                + (1.0 / 3) * self.d(temperature) * (Pr**3 - P0r**3)
+            )
+            * self.GAS_CONSTANT
+            * temperature
+        )
+
+        return volume_integral
+
+
+@dataclass(kw_only=True)
+class ShiSaxenaLowPressure(ShiSaxenaABC):
+    """Low pressure (< 1 kbar)."""
+
+    def _get_compressibility_coefficient(
+        self, temperature: float, coefficients: tuple[float, ...]
+    ) -> float:
+        """General form of the coefficients for the compressibility calculation.
+
+        Args:
+            temperature: Temperature in kelvin.
+            coefficients: Tuple of the coefficients a, b, c, or d.
+
+        Returns
+            The relevant coefficient.
+        """
+        Tr: float = self.reduced_temperature(temperature)
+        coefficient: float = (
+            coefficients[0]
+            + coefficients[1] / Tr
+            + coefficients[2] / Tr ** (3 / 2)
+            + coefficients[3] / Tr**3
+            + coefficients[4] / Tr**4
+        )
+
+        return coefficient
+
+
+@dataclass(kw_only=True)
+class ShiSaxenaHighPressure(ShiSaxenaABC):
+    """High pressure (>=1 kbar)."""
+
+    def _get_compressibility_coefficient(
+        self, temperature: float, coefficients: tuple[float, ...]
+    ) -> float:
+        """General form of the coefficients for the compressibility calculation.
+
+        Args:
+            temperature: Temperature in kelvin.
+            coefficients: Tuple of the coefficients a, b, c, or d.
+
+        Returns
+            The relevant coefficient.
+        """
+        Tr: float = self.reduced_temperature(temperature)
+        coefficient: float = (
+            coefficients[0]
+            + coefficients[1] * Tr
+            + coefficients[2] / Tr
+            + coefficients[3] * Tr**2
+            + coefficients[4] / Tr**2
+            + coefficients[5] * Tr**3
+            + coefficients[6] / Tr**3
+            + coefficients[7] * np.log(Tr)
+        )
+
+        return coefficient
