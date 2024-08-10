Jacobian calculation and bug fix reaction matrix

atmodeller/reaction_network_jax.py
@@ -23,11 +23,13 @@ from __future__ import annotations
 import logging
 import pprint
 import sys
+from typing import Callable
 
 import jax
 import jax.numpy as jnp
+import numpy as np
 import optimistix as optx
-from jax import Array
+from jax import Array, lax
 from jaxtyping import ArrayLike
 from optimistix._solution import Solution as Solution_optx
 
@@ -94,6 +96,8 @@ class ReactionNetwork:
                 except KeyError:
                     count = 0
                 formula_matrix = formula_matrix.at[element_index, species_index].set(count)
+
+        logger.debug("formula_matrix = %s", formula_matrix)
 
         return formula_matrix
 
@@ -366,6 +370,22 @@ class ReactionNetwork:
 
         return residual
 
+    def jacobian(self, args) -> Callable:
+        """Creates a Jacobian callable function.
+
+        Args:
+            args: Tuple of other arguments
+
+        Returns:
+            A callable function that computes the Jacobian with respect to `solution_array`.
+        """
+
+        # Partially apply `args` to the `objective_function`
+        def wrapped_objective(solution_array):
+            return self.objective_function(solution_array, args)
+
+        return jax.jacobian(wrapped_objective)
+
     def get_residual(self, solution: Solution, constraints: SystemConstraints) -> Array:
         """Gets the residual
 
@@ -398,7 +418,7 @@ class ReactionNetwork:
         *,
         constraints: SystemConstraints,
         tol: float = 1.0e-8,
-    ) -> tuple[Solution_optx, Solution]:
+    ) -> tuple[Solution_optx, Callable, Solution]:
 
         if initial_solution is None:
             initial_solution = InitialSolutionDict(species=self._species)
@@ -422,12 +442,18 @@ class ReactionNetwork:
         solution.planet = self._planet
         solution.value = jnp.array(sol.value)
 
-        print("sol = ", sol)
-        result = sol.result
-        print("string = ", str(result._value))
-        print("result = ", optx.RESULTS[result])
+        residual = self.get_residual(solution, constraints)
+        rmse = np.sqrt(np.sum(np.array(residual) ** 2))
+        # Success is indicated by no message
+        if optx.RESULTS[sol.result] == "":
+            logger.info("Success. RMSE = %0.2e, steps = %d", rmse, sol.stats["num_steps"])
+            logger.info("Solution = %s", pprint.pformat(solution.output_solution()))
 
-        return sol, solution
+        jacobian: Callable = self.jacobian((constraints,))
+
+        logger.info("Jacobian = %s", jacobian(solution.value))
+
+        return sol, jacobian, solution
 
 
 class ReactionNetworkWithCondensateStability(ReactionNetwork):
@@ -509,60 +535,147 @@ class ReactionNetworkWithCondensateStability(ReactionNetwork):
 
 
 def partial_rref(matrix: Array) -> Array:
-    """Computes the partial reduced row echelon form to determine linear components
+    """Computes the partial reduced row echelon form to determine linear components.
 
     Args:
-        matrix: The matrix to compute the reduced row echelon form
+        matrix: The matrix to compute the reduced row echelon form.
 
     Returns:
-        A matrix of linear components
+        A matrix of linear components.
     """
     nrows, ncols = matrix.shape
 
     # Augment the matrix with the identity matrix
-    augmented_matrix = jnp.hstack((matrix, jnp.eye(nrows)))
+    augmented_matrix: Array = jnp.hstack((matrix, jnp.eye(nrows)))
     logger.debug("augmented_matrix = \n%s", augmented_matrix)
 
+    def swap_rows(matrix: Array, row1: ArrayLike, row2: ArrayLike) -> Array:
+        """Swaps two rows in the matrix."""
+        # Extract rows to swap
+        row1_data: Array = lax.dynamic_slice(matrix, (row1, 0), (1, matrix.shape[1]))
+        row2_data: Array = lax.dynamic_slice(matrix, (row2, 0), (1, matrix.shape[1]))
+        # Update matrix
+        matrix = lax.dynamic_update_slice(matrix, row2_data, (row1, 0))
+        matrix = lax.dynamic_update_slice(matrix, row1_data, (row2, 0))
+
+        return matrix
+
     # Forward elimination
-    for i in range(ncols):
+    def forward_step(i: int, matrix: Array) -> Array:
         # Check if the pivot element is zero and swap rows to get a non-zero pivot element.
-        if augmented_matrix[i, i] == 0:
-            nonzero_rows = jnp.nonzero(augmented_matrix[i:, i])[0]
+        pivot_value: Array = lax.dynamic_slice(matrix, (i, i), (1, 1))
+        if pivot_value == 0:
+            nonzero_rows: Array = jnp.nonzero(matrix[i:, i])[0]
             if nonzero_rows.size > 0:
-                nonzero_row = nonzero_rows[0] + i
-                # Swap rows
-                augmented_matrix = augmented_matrix.at[i, :].set(augmented_matrix[nonzero_row, :])
-                augmented_matrix = augmented_matrix.at[nonzero_row, :].set(augmented_matrix[i, :])
+                nonzero_row: Array = nonzero_rows[0] + i
+                matrix = swap_rows(matrix, i, nonzero_row)
 
         # Perform row operations to eliminate values below the pivot.
         for j in range(i + 1, nrows):
-            ratio = augmented_matrix[j, i] / augmented_matrix[i, i]
-            augmented_matrix = augmented_matrix.at[j, :].set(
-                augmented_matrix[j, :] - ratio * augmented_matrix[i, :]
-            )
+            pivot: Array = lax.dynamic_slice(matrix, (i, i), (1, 1))[0, 0]
+            ratio: Array = lax.dynamic_slice(matrix, (j, i), (1, 1))[0, 0] / pivot
+            row_i: Array = lax.dynamic_slice(matrix, (i, 0), (1, ncols + nrows))
+            row_j: Array = lax.dynamic_slice(matrix, (j, 0), (1, ncols + nrows))
+            matrix = lax.dynamic_update_slice(matrix, row_j - ratio * row_i, (j, 0))
 
-    logger.debug("augmented_matrix after forward elimination = \n%s", augmented_matrix)
+        return matrix
+
+    for i in range(ncols):
+        augmented_matrix = forward_step(i, augmented_matrix)
+
+    logger.warning("augmented_matrix after forward step = \n%s", augmented_matrix)
 
     # Backward substitution
-    for i in range(ncols - 1, -1, -1):
+    def backward_step(i: int, matrix: Array):
         # Normalize the pivot row.
-        augmented_matrix = augmented_matrix.at[i, :].set(
-            augmented_matrix[i, :] / augmented_matrix[i, i]
-        )
+        pivot: Array = lax.dynamic_slice(matrix, (i, i), (1, 1))[0, 0]
+        logger.warning("pivot = %s", pivot)
+        normalized_row = lax.dynamic_slice(matrix, (i, 0), (1, ncols + nrows)) / pivot
+        logger.warning("normalized_row = %s", normalized_row)
+        matrix = lax.dynamic_update_slice(matrix, normalized_row, (i, 0))
+        logger.warning("matrix = %s", matrix)
         # Eliminate values above the pivot.
         for j in range(i - 1, -1, -1):
-            if augmented_matrix[j, i] != 0:
-                ratio = augmented_matrix[j, i] / augmented_matrix[i, i]
-                augmented_matrix = augmented_matrix.at[j, :].set(
-                    augmented_matrix[j, :] - ratio * augmented_matrix[i, :]
-                )
+            if lax.dynamic_slice(matrix, (j, i), (1, 1))[0, 0] != 0:
+                # Scaled pivot row.
+                pivot = lax.dynamic_slice(matrix, (i, i), (1, 1))[0, 0]
+                ratio: Array = lax.dynamic_slice(matrix, (j, i), (1, 1))[0, 0] / pivot
+                logger.warning("j=%d not zero and ratio = %s", j, ratio)
+                row_i: Array = lax.dynamic_slice(matrix, (i, 0), (1, ncols + nrows))
+                row_j: Array = lax.dynamic_slice(matrix, (j, 0), (1, ncols + nrows))
+                matrix = lax.dynamic_update_slice(matrix, row_j - ratio * row_i, (j, 0))
 
-    logger.debug("augmented_matrix after backward substitution = \n%s", augmented_matrix)
+        return matrix
 
-    # Extract the reduced matrix and component matrix
-    reduced_matrix = augmented_matrix[:, :ncols]
-    component_matrix = augmented_matrix[ncols:, ncols:]
+    for i in range(ncols - 1, -1, -1):
+        augmented_matrix = backward_step(i, augmented_matrix)
+
+    logger.warning("augmented_matrix after backward step = \n%s", augmented_matrix)
+
+    reduced_matrix = lax.dynamic_slice(augmented_matrix, (0, 0), (nrows, ncols))
+    component_matrix = lax.dynamic_slice(augmented_matrix, (ncols, ncols), (nrows - ncols, nrows))
     logger.debug("reduced_matrix = \n%s", reduced_matrix)
     logger.debug("component_matrix = \n%s", component_matrix)
 
     return component_matrix
+
+
+# def partial_rref_numpy(matrix: Array) -> Array:
+#     """Computes the partial reduced row echelon form to determine linear components
+
+#     Args:
+#         matrix: The matrix to compute the reduced row echelon form
+
+#     Returns:
+#         A matrix of linear components
+#     """
+#     nrows, ncols = matrix.shape
+
+#     # Augment the matrix with the identity matrix
+#     augmented_matrix: Array = jnp.hstack((matrix, jnp.eye(nrows)))
+#     logger.debug("augmented_matrix = \n%s", augmented_matrix)
+
+#     # Forward elimination
+#     for i in range(ncols):
+#         # Check if the pivot element is zero and swap rows to get a non-zero pivot element.
+#         if augmented_matrix[i, i] == 0:
+#             nonzero_rows: Array = jnp.nonzero(augmented_matrix[i:, i])[0]
+#             if nonzero_rows.size > 0:
+#                 nonzero_row = nonzero_rows[0] + i
+#                 # Swap rows
+#                 augmented_matrix = augmented_matrix.at[i, :].set(augmented_matrix[nonzero_row, :])
+#                 augmented_matrix = augmented_matrix.at[nonzero_row, :].set(augmented_matrix[i, :])
+#         logger.warning("after pivot swap = %s", augmented_matrix)
+
+#         # Perform row operations to eliminate values below the pivot.
+#         for j in range(i + 1, nrows):
+#             ratio = augmented_matrix[j, i] / augmented_matrix[i, i]
+#             augmented_matrix = augmented_matrix.at[j, :].set(
+#                 augmented_matrix[j, :] - ratio * augmented_matrix[i, :]
+#             )
+
+#     logger.debug("augmented_matrix after forward elimination = \n%s", augmented_matrix)
+
+#     # Backward substitution
+#     for i in range(ncols - 1, -1, -1):
+#         # Normalize the pivot row.
+#         augmented_matrix = augmented_matrix.at[i, :].set(
+#             augmented_matrix[i, :] / augmented_matrix[i, i]
+#         )
+#         # Eliminate values above the pivot.
+#         for j in range(i - 1, -1, -1):
+#             if augmented_matrix[j, i] != 0:
+#                 ratio = augmented_matrix[j, i] / augmented_matrix[i, i]
+#                 augmented_matrix = augmented_matrix.at[j, :].set(
+#                     augmented_matrix[j, :] - ratio * augmented_matrix[i, :]
+#                 )
+
+#     logger.debug("augmented_matrix after backward substitution = \n%s", augmented_matrix)
+
+#     # Extract the reduced matrix and component matrix
+#     reduced_matrix = augmented_matrix[:, :ncols]
+#     component_matrix = augmented_matrix[ncols:, ncols:]
+#     logger.debug("reduced_matrix = \n%s", reduced_matrix)
+#     logger.debug("component_matrix = \n%s", component_matrix)
+
+#     return component_matrix