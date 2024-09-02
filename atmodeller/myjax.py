0c6bbfed2f2433ddceb3d1c2f8f7e68d4a2ee5ac
Dan J Bower <djbower@users.noreply.github.com>
2024-09-02 15:46:37 +0200

    More thought into traced versus static arrays

--- atmodeller/myjax.py
+++ atmodeller/myjax.py
@@ -21,16 +21,16 @@ Most likely these will migrate elsewhere in the code base eventually.
 # Convenient to use chemical symbol names so pylint: disable=invalid-name
 
 import sys
-from functools import partial
 from typing import NamedTuple
 
 import jax.numpy as jnp
+import numpy as np
+import numpy.typing as npt
 import pandas as pd
-from jax import Array, jit
+from jax import jit
 from molmass import Formula
 
-from atmodeller.reaction_network import partial_rref
-from atmodeller.utilities import unit_conversion
+from atmodeller.utilities import partial_rref, unit_conversion
 
 if sys.version_info < (3, 11):
     from typing_extensions import Self
@@ -49,10 +49,10 @@ class SpeciesData(NamedTuple):
     composition: dict[str, int]
     hill_formula: str
     molar_mass: float
-    gibbs_coefficients: Array
+    gibbs_coefficients: tuple[float, ...]
 
     @classmethod
-    def create(cls, formula: str, phase: str, gibbs_coefficients: Array) -> Self:
+    def create(cls, formula: str, phase: str, gibbs_coefficients: tuple[float, ...]) -> Self:
         _mformula: Formula = Formula(formula)
         _composition_df: pd.DataFrame = _mformula.composition().dataframe()
         # Linter complaining but seems fine so pylint: disable=unsubscriptable-object
@@ -75,9 +75,9 @@ class SpeciesData(NamedTuple):
         return sum(self.composition.values())
 
     @property
-    def elements(self) -> list[str]:
+    def elements(self) -> tuple[str, ...]:
         """Elements"""
-        return list(self.composition.keys())
+        return tuple(self.composition.keys())
 
     @property
     def name(self) -> str:
@@ -85,8 +85,8 @@ class SpeciesData(NamedTuple):
         return f"{self.hill_formula}_{self.phase}"
 
 
-@partial(jit, static_argnames="temperature")
-def gibbs_energy_of_formation(species_data: SpeciesData, temperature: float) -> Array:
+@jit
+def gibbs_energy_of_formation(species_data: SpeciesData, temperature: float) -> npt.NDArray:
     r"""Gibbs energy of formation
 
     Args:
@@ -96,12 +96,12 @@ def gibbs_energy_of_formation(species_data: SpeciesData, temperature: float) -> 
     Returns:
         The standard Gibbs free energy of formation in :math:`\mathrm{J}\mathrm{mol}^{-1}`
     """
-    gibbs: Array = (
+    gibbs: npt.NDArray = (
         species_data.gibbs_coefficients[0] / temperature
-        + species_data.gibbs_coefficients[1] * jnp.log(temperature)
+        + species_data.gibbs_coefficients[1] * np.log(temperature)
         + species_data.gibbs_coefficients[2]
-        + species_data.gibbs_coefficients[3] * jnp.power(temperature, 1)
-        + species_data.gibbs_coefficients[4] * jnp.power(temperature, 2)
+        + species_data.gibbs_coefficients[3] * np.power(temperature, 1)
+        + species_data.gibbs_coefficients[4] * np.power(temperature, 2)
     )
 
     return gibbs * 1000.0  # kilo
@@ -109,19 +109,19 @@ def gibbs_energy_of_formation(species_data: SpeciesData, temperature: float) -> 
 
 # For all below fits, "zero" temperature was set to 0.01 to avoid problems with fitting a/T
 number_of_coefficients: int = 5
-reference_gibbs: Array = jnp.zeros(number_of_coefficients)
+reference_gibbs: tuple[float, ...] = (0,) * 5
 
 CH4_g: SpeciesData = SpeciesData.create(
-    "CH4", "g", jnp.array([-7.471666e-01, -9.118137e00, -3.418606e01, 1.176984e-01, -4.291384e-07])
+    "CH4", "g", (-7.471666e-01, -9.118137e00, -3.418606e01, 1.176984e-01, -4.291384e-07)
 )
 Cl2_g: SpeciesData = SpeciesData.create("Cl2", "g", reference_gibbs)
 CO_g: SpeciesData = SpeciesData.create(
-    "CO", "g", jnp.array([-1.413961e-01, -1.125331e00, -1.048469e02, -8.915501e-02, 1.503998e-06])
+    "CO", "g", (-1.413961e-01, -1.125331e00, -1.048469e02, -8.915501e-02, 1.503998e-06)
 )
 CO2_g: SpeciesData = SpeciesData.create(
     "CO2",
     "g",
-    jnp.array([-1.884621e-02, -2.387862e-01, -3.923660e02, -2.506878e-03, 7.017329e-07]),
+    (-1.884621e-02, -2.387862e-01, -3.923660e02, -2.506878e-03, 7.017329e-07),
 )
 C_cr: SpeciesData = SpeciesData.create("C", "cr", reference_gibbs)
 H2_g: SpeciesData = SpeciesData.create(
@@ -132,16 +132,16 @@ H2_g: SpeciesData = SpeciesData.create(
 H2O_g: SpeciesData = SpeciesData.create(
     "H2O",
     "g",
-    jnp.array([-3.817134e-01, -4.469468e00, -2.213329e02, 5.975648e-02, 6.535070e-08]),
+    (-3.817134e-01, -4.469468e00, -2.213329e02, 5.975648e-02, 6.535070e-08),
 )
 H2O_l: SpeciesData = SpeciesData.create(
     "H2O",
     "l",
-    jnp.array([-9.885210e02, -3.519502e00, -2.658921e02, 1.856359e-01, -3.631301e-05]),
+    (-9.885210e02, -3.519502e00, -2.658921e02, 1.856359e-01, -3.631301e-05),
 )
 N2_g: SpeciesData = SpeciesData.create("N2", "g", reference_gibbs)
 NH3_g: SpeciesData = SpeciesData.create(
-    "NH3", "g", jnp.array([-4.493772e-01, -5.741781e00, -2.041235e01, 1.233233e-01, -9.071982e-07])
+    "NH3", "g", (-4.493772e-01, -5.741781e00, -2.041235e01, 1.233233e-01, -9.071982e-07)
 )
 O2_g: SpeciesData = SpeciesData.create("O2", "g", reference_gibbs)
 
@@ -155,6 +155,9 @@ class ReactionNetworkJAX:
     """Primary role is to assemble Python objects to generate JAX-compliants arrays for numerical
     solution. In this regard, this should only generate arrays that are required once, and not
     computed dynamically during the solve.
+
+    Ironically, since the species list will be treated as a static argument, this will all use
+    numpy since nothing here is traced.
     """
 
     @staticmethod
@@ -175,7 +178,7 @@ class ReactionNetworkJAX:
 
         return tuple(sorted_elements)
 
-    def formula_matrix(self, species: list[SpeciesData]) -> Array:
+    def formula_matrix(self, species: list[SpeciesData]) -> npt.NDArray:
         """Formula matrix
 
         Elements are given in rows and species in columns following the convention in
@@ -186,18 +189,20 @@ class ReactionNetworkJAX:
         """
         unique_elements: tuple[str, ...] = self.unique_elements_in_species(species)
 
-        formula_matrix: Array = jnp.zeros((len(unique_elements), len(species)), dtype=jnp.int_)
+        formula_matrix: npt.NDArray = np.zeros(
+            (len(unique_elements), len(species)), dtype=jnp.int_
+        )
         for element_index, element in enumerate(unique_elements):
             for species_index, species_ in enumerate(species):
                 try:
                     count: int = species_.composition[element]
                 except KeyError:
                     count = 0
-                formula_matrix = formula_matrix.at[element_index, species_index].set(count)
+                formula_matrix[element_index, species_index] = count
 
         return formula_matrix
 
-    def reaction_matrix(self, species: list[SpeciesData]) -> Array:
+    def reaction_matrix(self, species: list[SpeciesData]) -> npt.NDArray:
         """Reaction matrix
 
         Returns:
@@ -208,7 +213,7 @@ class ReactionNetworkJAX:
         #    logger.debug("Only one species therefore no reactions")
         #    return None
 
-        transpose_formula_matrix: Array = self.formula_matrix(species).T
+        transpose_formula_matrix: npt.NDArray = self.formula_matrix(species).T
 
         return partial_rref(transpose_formula_matrix)
 
@@ -220,7 +225,7 @@ def test():
 
     reaction_network: ReactionNetworkJAX = ReactionNetworkJAX()
 
-    formula_matrix: Array = reaction_network.formula_matrix(species_list)
+    formula_matrix: npt.NDArray = reaction_network.formula_matrix(species_list)
 
     print(formula_matrix)
 
