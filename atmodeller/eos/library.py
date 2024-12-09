9166e86650c5824391ce525fbe3fb54671d90ba8
Dan J Bower <djbower@users.noreply.github.com>
2024-12-09 16:33:11 +0100

    Holley EOS models

--- atmodeller/eos/library.py
+++ atmodeller/eos/library.py
@@ -28,11 +28,11 @@ Usage:
 import logging
 from pathlib import Path
 
-from atmodeller.constants import ATMOSPHERE
 from atmodeller.eos._holland_powell import get_holland_eos_models
-from atmodeller.eos.classes import BeattieBridgeman, Chabrier
+from atmodeller.eos._holley import get_holley_eos_models
+from atmodeller.eos.classes import Chabrier
 from atmodeller.eos.core import RealGas, RealGasBounded
-from atmodeller.utilities import ExperimentalCalibrationNew, unit_conversion
+from atmodeller.utilities import ExperimentalCalibrationNew
 
 logger: logging.Logger = logging.getLogger(__name__)
 
@@ -55,124 +55,6 @@ H2_He_Y0297_chabrier21: RealGas = Chabrier(Path("TABLEEOS_2021_TP_Y0297_v1"))
 
 # endregion
 
-# region: Holley et al. (1958)
-
-# Coefficients from Table I, which must be converted to the correct units scheme (SI and pressure
-# in bar). Using the original values in the paper also facilitates visual comparison and checking.
-
-
-def volume_conversion(x: float) -> float:
-    """Volume conversion for :cite:t:`HWZ58` units."""
-    return x * unit_conversion.litre_to_m3
-
-
-def A0_conversion(x: float) -> float:
-    """PV**2 conversion for :cite:t:`HWZ58` units."""
-    return x * ATMOSPHERE * unit_conversion.litre_to_m3**2
-
-
-def atm2bar(x: float) -> float:
-    """Atmosphere to bar conversion."""
-    return unit_conversion.atmosphere_to_bar * x
-
-
-H2_beattie_holley58: RealGas = BeattieBridgeman(
-    A0=A0_conversion(0.1975),
-    a=volume_conversion(-0.00506),
-    B0=volume_conversion(0.02096),
-    b=volume_conversion(-0.04359),
-    c=volume_conversion(0.0504e4),
-)
-"""H2 Beattie-Bridgeman :cite:p:`HWZ58`"""
-H2_beattie_holley58_bounded: RealGas = RealGasBounded(
-    H2_beattie_holley58,
-    ExperimentalCalibrationNew(100, 1000, atm2bar(0.1), atm2bar(1000)),
-)
-"""H2 Beattie-Bridgeman bounded :cite:p:`HWZ58`"""
-
-N2_beattie_holley58: RealGas = BeattieBridgeman(
-    A0=A0_conversion(1.3445),
-    a=volume_conversion(0.02617),
-    B0=volume_conversion(0.05046),
-    b=volume_conversion(-0.00691),
-    c=volume_conversion(4.2e4),
-)
-"""N2 Beattie-Bridgeman :cite:p:`HWZ58`"""
-N2_beattie_holley58_bounded: RealGas = RealGasBounded(
-    N2_beattie_holley58, ExperimentalCalibrationNew(200, 1000, atm2bar(0.1), atm2bar(1000))
-)
-"""N2 Beattie-Bridgeman bounded :cite:p:`HWZ58`"""
-
-O2_beattie_holley58: RealGas = BeattieBridgeman(
-    A0=A0_conversion(1.4911),
-    a=volume_conversion(0.02562),
-    B0=volume_conversion(0.04624),
-    b=volume_conversion(0.004208),
-    c=volume_conversion(4.8e4),
-)
-"""O2 Beattie-Bridgeman :cite:p:`HWZ58`"""
-O2_beattie_holley58_bounded: RealGas = RealGasBounded(
-    O2_beattie_holley58, ExperimentalCalibrationNew(200, 1000, atm2bar(0.1), atm2bar(1000))
-)
-"""O2 Beattie-Bridgeman bounded :cite:p:`HWZ58`"""
-
-CO2_beattie_holley58: RealGas = BeattieBridgeman(
-    A0=A0_conversion(5.0065),
-    a=volume_conversion(0.07132),
-    B0=volume_conversion(0.10476),
-    b=volume_conversion(0.07235),
-    c=volume_conversion(66e4),
-)
-"""CO2 Beattie-Bridgeman :cite:p:`HWZ58`"""
-CO2_beattie_holley58_bounded: RealGas = RealGasBounded(
-    CO2_beattie_holley58,
-    ExperimentalCalibrationNew(400, 1000, atm2bar(0.1), atm2bar(1000)),
-)
-"""CO2 Beattie-Bridgeman bounded :cite:p:`HWZ58`"""
-
-NH3_beattie_holley58: RealGas = BeattieBridgeman(
-    A0=A0_conversion(2.3930),
-    a=volume_conversion(0.17031),
-    B0=volume_conversion(0.03415),
-    b=volume_conversion(0.19112),
-    c=volume_conversion(476.87e4),
-)
-"""NH3 Beattie-Bridgeman :cite:p:`HWZ58`"""
-NH3_beattie_holley58_bounded: RealGas = RealGasBounded(
-    NH3_beattie_holley58, ExperimentalCalibrationNew(500, 1000, atm2bar(0.1), atm2bar(500))
-)
-"""NH3 Beattie-Bridgeman bounded :cite:p:`HWZ58`"""
-
-CH4_beattie_holley58: RealGas = BeattieBridgeman(
-    A0=A0_conversion(2.2769),
-    a=volume_conversion(0.01855),
-    B0=volume_conversion(0.05587),
-    b=volume_conversion(-0.01587),
-    c=volume_conversion(12.83e4),
-)
-"""CH4 Beattie-Bridgeman :cite:p:`HWZ58`"""
-CH4_beattie_holley58_bounded: RealGas = RealGasBounded(
-    CH4_beattie_holley58,
-    ExperimentalCalibrationNew(200, 1000, atm2bar(0.1), atm2bar(1000)),
-)
-"""CH4 Beattie-Bridgeman bounded :cite:p:`HWZ58`"""
-
-He_beattie_holley58: RealGas = BeattieBridgeman(
-    A0=A0_conversion(0.0216),
-    a=volume_conversion(0.05984),
-    B0=volume_conversion(0.01400),
-    b=0,
-    c=volume_conversion(0.004e4),
-)
-"""He Beattie-Bridgeman :cite:p:`HWZ58`"""
-He_beattie_holley58_bounded: RealGas = RealGasBounded(
-    He_beattie_holley58,
-    ExperimentalCalibrationNew(100, 1000, atm2bar(0.1), atm2bar(1000)),
-)
-"""He Beattie-Bridgeman bounded :cite:p:`HWZ58`"""
-
-# endregion
-
 
 def get_eos_models() -> dict[str, RealGas]:
     """Gets a dictionary of EOS models
@@ -187,28 +69,16 @@ def get_eos_models() -> dict[str, RealGas]:
         Dictionary of EOS models
     """
     eos_models: dict[str, RealGas] = {}
-    eos_models["CH4_beattie_holley58"] = CH4_beattie_holley58
-    eos_models["CH4_beattie_holley58_bounded"] = CH4_beattie_holley58_bounded
-    eos_models["CO2_beattie_holley58"] = CO2_beattie_holley58
-    eos_models["CO2_beattie_holley58_bounded"] = CO2_beattie_holley58_bounded
-    eos_models["H2_beattie_holley58"] = H2_beattie_holley58
-    eos_models["H2_beattie_holley58_bounded"] = H2_beattie_holley58_bounded
     eos_models["H2_chabrier21"] = H2_chabrier21
     eos_models["H2_chabrier21_bounded"] = H2_chabrier21_bounded
     eos_models["H2_He_Y0275_chabrier21"] = H2_He_Y0275_chabrier21
     eos_models["H2_He_Y0292_chabrier21"] = H2_He_Y0292_chabrier21
     eos_models["H2_He_Y0297_chabrier21"] = H2_He_Y0297_chabrier21
-    eos_models["He_beattie_holley58"] = He_beattie_holley58
-    eos_models["He_beattie_holley58_bounded"] = He_beattie_holley58_bounded
     eos_models["He_chabrier21"] = He_chabrier21
-    eos_models["N2_beattie_holley58"] = N2_beattie_holley58
-    eos_models["N2_beattie_holley58_bounded"] = N2_beattie_holley58_bounded
-    eos_models["NH3_beattie_holley58"] = NH3_beattie_holley58
-    eos_models["NH3_beattie_holley58_bounded"] = NH3_beattie_holley58_bounded
-    eos_models["O2_beattie_holley58"] = O2_beattie_holley58
-    eos_models["O2_beattie_holley58_bounded"] = O2_beattie_holley58_bounded
 
-    # Merge in Holland and Powell models
+    # Merge Holley models
+    eos_models |= get_holley_eos_models()
+    # Merge Holland and Powell models
     eos_models |= get_holland_eos_models()
 
     return eos_models
