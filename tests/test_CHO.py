32e9d470db7b30603ca23bb95a1a28de23f14cad
Dan J Bower <djbower@users.noreply.github.com>
2025-04-27 14:14:31 +0200

    Update test_CHO.py

--- tests/test_CHO.py
+++ tests/test_CHO.py
@@ -160,7 +160,9 @@ def test_H_fO2_batch_temperature(helper) -> None:
     interior_atmosphere: InteriorAtmosphere = InteriorAtmosphere(species)
 
     # Number of surface temperatures is different to number of species to test array shapes work.
-    surface_temperatures: npt.NDArray[np.float_] = np.array([1500, 2000, 2500, 3000])
+    surface_temperatures: npt.NDArray[np.float_] = np.array(
+        [1500, 2000, 2500, 3000], dtype=np.float_
+    )
     planet: Planet = Planet(surface_temperature=surface_temperatures)
 
     fugacity_constraints: dict[str, FugacityConstraintProtocol] = {"O2_g": IronWustiteBuffer()}
