fa6a41934e76553f06dc0cc1bfd32e7da6e1cdba
Dan J Bower <djbower@users.noreply.github.com>
2025-07-31 21:14:13 +0200

    Comment out EOS to allow tests to load

--- atmodeller/solubility/library.py
+++ atmodeller/solubility/library.py
@@ -35,8 +35,8 @@ from atmodeller.solubility._carbon_species import (
 from atmodeller.solubility._hydrogen_species import (
     H2_andesite_hirschmann12,
     H2_basalt_hirschmann12,
-    H2_chachan18,
-    H2_kite19,
+    # H2_chachan18,
+    # H2_kite19,
     H2_silicic_melts_gaillard03,
     H2O_ano_dio_newcombe17,
     H2O_basalt_dixon95,
@@ -89,8 +89,8 @@ def get_solubility_models() -> dict[str, Solubility]:
     models["H2_andesite_hirschmann12"] = H2_andesite_hirschmann12
     models["H2_basalt_hirschmann12"] = H2_basalt_hirschmann12
     models["H2_silicic_melts_gaillard03"] = H2_silicic_melts_gaillard03
-    models["H2_chachan18"] = H2_chachan18
-    models["H2_kite19"] = H2_kite19
+    # models["H2_chachan18"] = H2_chachan18
+    # models["H2_kite19"] = H2_kite19
     models["H2O_ano_dio_newcombe17"] = H2O_ano_dio_newcombe17
     models["H2O_basalt_dixon95"] = H2O_basalt_dixon95
     models["H2O_basalt_mitchell17"] = H2O_basalt_mitchell17
