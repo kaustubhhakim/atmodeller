7ccf319cc0cf2f48b580ac481f1941917bf292ad
Dan J Bower <djbower@users.noreply.github.com>
2025-08-18 13:42:24 +0200

    fix: PRESSURE_REFERENCE import

--- atmodeller/eos/_wang.py
+++ atmodeller/eos/_wang.py
@@ -26,8 +26,8 @@ import equinox as eqx
 import jax.numpy as jnp
 from jaxtyping import Array, ArrayLike
 
-from atmodeller import PRESSURE_REFERENCE, override
-from atmodeller.constants import GAS_CONSTANT_BAR
+from atmodeller import override
+from atmodeller.constants import GAS_CONSTANT_BAR, PRESSURE_REFERENCE
 from atmodeller.eos._aggregators import CombinedRealGas
 from atmodeller.eos.core import RealGas
 from atmodeller.utilities import ExperimentalCalibration, to_native_floats
