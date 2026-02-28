a6a89444d53ea9e5628dc9f0fafb1ff73df02542
Dan J Bower <djbower@users.noreply.github.com>
2026-02-28 08:52:45 +0100

    test: update

--- tests/test_condensates.py
+++ tests/test_condensates.py
@@ -5,6 +5,7 @@
 """Tests for C-H-O systems with stable or unstable condensates"""
 
 import logging
+from typing import Any
 
 import numpy as np
 from jaxtyping import ArrayLike
@@ -14,13 +15,12 @@ from atmodeller import debug_logger
 from atmodeller.classes import EquilibriumModel
 from atmodeller.containers import ChemicalSpecies, Planet, ThermodynamicState
 from atmodeller.interfaces import FugacityConstraintProtocol
-from atmodeller.output_core import Output
 from atmodeller.phases import GasPhase, PurePhase
 from atmodeller.thermodata import IronWustiteBuffer
 from atmodeller.utilities import earth_oceans_to_hydrogen_mass
 
 logger: logging.Logger = debug_logger()
-logger.setLevel(logging.WARNING)
+logger.setLevel(logging.INFO)
 
 RTOL: float = 1.0e-8
 """Relative tolerance"""
@@ -35,7 +35,7 @@ graphite: PurePhase = PurePhase.create("C", state="s")
 CHO_model: EquilibriumModel = EquilibriumModel(gas, condensates=(graphite,))
 
 
-def test_graphite_stable(helper) -> None:
+def test_graphite_stable() -> None:
     """Tests graphite stable with around 50% condensed C mass fraction"""
 
     planet: Planet = Planet(temperature=873)
@@ -49,29 +49,27 @@ def test_graphite_stable(helper) -> None:
     mass_constraints: dict[str, ArrayLike] = {"C": c_kg, "H": h_kg, "O": o_kg}
 
     CHO_model.solve(
-        state=planet,
-        fugacity_constraints=fugacity_constraints,
-        mass_constraints=mass_constraints,
-        solver="basic",
+        state=planet, fugacity_constraints=fugacity_constraints, mass_constraints=mass_constraints
     )
-    output: Output = CHO_model.output  # noqa: F821
-    solution: dict[str, ArrayLike] = output.quick_look()
 
-    factsage_result: dict[str, float] = {
-        "O2_g": 1.27e-25,
-        "H2_g": 14.564,
-        "CO_g": 0.07276,
-        "H2O_g": 4.527,
-        "CO2_g": 0.061195,
-        "CH4_g": 96.74,
-        "C_cd_activity": 1.0,
-        "mass_C_cd": 3.54162e20,
+    factsage_result: dict[str, Any] = {
+        "gas": {
+            "partial_pressure_bar": {
+                "O2_g": 1.27e-25,
+                "H2_g": 14.564,
+                "CO_g": 0.07276,
+                "H2O_g": 4.527,
+                "CO2_g": 0.061195,
+                "CH4_g": 96.74,
+            }
+        },
+        "condensates": {"activity": {"C_s": 1.0}, "mass_kg": {"C_s": 3.54162e20}},
     }
 
-    assert helper.isclose(solution, factsage_result, log=True, rtol=TOLERANCE, atol=TOLERANCE)
+    assert CHO_model.output.compare(factsage_result, log=True, rtol=TOLERANCE, atol=TOLERANCE)
 
 
-def test_graphite_unstable(helper) -> None:
+def test_graphite_unstable() -> None:
     """Tests C-H-O system at IW+0.5 with graphite unstable
 
     Similar to :cite:p:`BHS22{Table E, row 2}`
@@ -87,24 +85,25 @@ def test_graphite_unstable(helper) -> None:
     CHO_model.solve(
         state=planet, fugacity_constraints=fugacity_constraints, mass_constraints=mass_constraints
     )
-    output: Output = CHO_model.output
-    solution: dict[str, ArrayLike] = output.quick_look()
 
-    factsage_result: dict[str, float] = {
-        "O2_g": 4.11e-13,
-        "H2_g": 236.98,
-        "CO_g": 46.42,
-        "H2O_g": 337.16,
-        "CO2_g": 30.88,
-        "CH4_g": 28.66,
-        "C_s_activity": 0.12202,
-        "mass_C_s": 0.0,
+    factsage_result: dict[str, Any] = {
+        "gas": {
+            "partial_pressure_bar": {
+                "O2_g": 4.11e-13,
+                "H2_g": 236.98,
+                "CO_g": 46.42,
+                "H2O_g": 337.16,
+                "CO2_g": 30.88,
+                "CH4_g": 28.66,
+            }
+        },
+        "condensates": {"activity": {"C_s": 0.12202}},
     }
 
-    assert helper.isclose(solution, factsage_result, log=True, rtol=TOLERANCE, atol=TOLERANCE)
+    assert CHO_model.output.compare(factsage_result, log=True, rtol=TOLERANCE, atol=TOLERANCE)
 
 
-def test_water_stable(helper) -> None:
+def test_water_stable() -> None:
     """Condensed water at 10 bar"""
 
     gas: GasPhase = GasPhase.create(("H2", "H2O", "O2"))
@@ -117,22 +116,17 @@ def test_water_stable(helper) -> None:
     o_kg: float = 1.14375e21
     mass_constraints: dict[str, ArrayLike] = {"H": h_kg, "O": o_kg}
 
-    model.solve(state=planet, mass_constraints=mass_constraints, solver="robust")
-    output: Output = model.output
-    solution: dict[str, ArrayLike] = output.quick_look()
+    model.solve(state=planet, mass_constraints=mass_constraints)
 
-    factsage_result: dict[str, float] = {
-        "H2O_g": 3.3596,
-        "H2_g": 6.5604,
-        "O2_g": 5.6433e-58,
-        "H2O_l_activity": 1.0,
-        "mass_H2O_l": 1.247201e21,
+    factsage_result: dict[str, Any] = {
+        "gas": {"partial_pressure_bar": {"H2O_g": 3.3596, "H2_g": 6.5604, "O2_g": 5.6433e-58}},
+        "condensates": {"activity": {"H2O_l": 1.0}, "mass_kg": {"H2O_l": 1.247201e21}},
     }
 
-    assert helper.isclose(solution, factsage_result, log=True, rtol=TOLERANCE, atol=TOLERANCE)
+    assert model.output.compare(factsage_result, log=True, rtol=TOLERANCE, atol=TOLERANCE)
 
 
-def test_graphite_water_stable(helper) -> None:
+def test_graphite_water_stable() -> None:
     """Tests C and water in equilibrium at 430 K and 10 bar"""
 
     gas: GasPhase = GasPhase.create(("H2O", "H2", "O2", "CO", "CO2", "CH4"))
@@ -146,27 +140,29 @@ def test_graphite_water_stable(helper) -> None:
     o_kg: float = 2.48298883581636e21
     mass_constraints: dict[str, ArrayLike] = {"C": c_kg, "H": h_kg, "O": o_kg}
 
-    model.solve(state=planet, mass_constraints=mass_constraints, solver="basic")
-    output: Output = model.output
-    solution: dict[str, ArrayLike] = output.quick_look()
+    model.solve(state=planet, mass_constraints=mass_constraints)
 
-    factsage_result: dict[str, float] = {
-        "CH4_g": 0.3241,
-        "CO2_g": 4.3064,
-        "CO_g": 2.77e-6,
-        "C_s_activity": 1.0,
-        "H2O_g": 5.3672,
-        "H2O_l_activity": 1.0,
-        "H2_g": 0.0023,
-        "O2_g": 4.74e-48,
-        "mass_C_s": 8.75101e19,
-        "mass_H2O_l": 2.74821e21,
+    factsage_result: dict[str, Any] = {
+        "gas": {
+            "partial_pressure_bar": {
+                "CH4_g": 0.3241,
+                "CO2_g": 4.3064,
+                "CO_g": 2.77e-6,
+                "H2_g": 0.0023,
+                "O2_g": 4.74e-48,
+                "H2O_g": 5.3672,
+            }
+        },
+        "condensates": {
+            "activity": {"C_s": 1.0, "H2O_l": 1.0},
+            "mass_kg": {"C_s": 8.75101e19, "H2O_l": 2.74821e21},
+        },
     }
 
-    assert helper.isclose(solution, factsage_result, log=True, rtol=TOLERANCE, atol=TOLERANCE)
+    assert model.output.compare(factsage_result, log=True, rtol=TOLERANCE, atol=TOLERANCE)
 
 
-def test_impose_stable(helper) -> None:
+def test_impose_stable() -> None:
     """Tests a user-imposed stable condensate"""
 
     # Since in this example we do not provide carbon in the injected gas stream, we cannot solve
@@ -192,18 +188,18 @@ def test_impose_stable(helper) -> None:
         key: value * Formula(key).mass for key, value in mole_fractions.items()
     }
 
-    # Solve
-    model.solve(state=state, mass_constraints=mass_constraints, solver="basic")
+    model.solve(state=state, mass_constraints=mass_constraints)
 
-    output: Output = model.output
-    solution: dict[str, ArrayLike] = output.quick_look()
-
-    factsage_result: dict[str, float] = {
-        "CH4_g": 0.000194708,
-        "C_s_activity": 1.0,
-        "H_g": 0.000201266,
-        "H2_g": 0.49807992,
-        "N2_g": 0.49866269,
+    factsage_result: dict[str, Any] = {
+        "gas": {
+            "partial_pressure_bar": {
+                "CH4_g": 0.000194708,
+                "H_g": 0.000201266,
+                "H2_g": 0.49807992,
+                "N2_g": 0.49866269,
+            }
+        },
+        "condensates": {"activity": {"C_s": 1.0}},
     }
 
-    assert helper.isclose(solution, factsage_result, log=True, rtol=TOLERANCE, atol=TOLERANCE)
+    assert model.output.compare(factsage_result, log=True, rtol=TOLERANCE, atol=TOLERANCE)
