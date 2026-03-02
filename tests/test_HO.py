8a12b8f200c54fc9c6210de71265608012a27b40
Dan J Bower <djbower@users.noreply.github.com>
2026-03-02 17:24:21 +0100

    test: update output

--- tests/test_HO.py
+++ tests/test_HO.py
@@ -35,7 +35,7 @@ solubility_models: Mapping[str, SolubilityProtocol] = get_solubility_models()
 
 gas: GasPhase = GasPhase.create(("H2O", "H2", "O2"))
 H2O_di: ReservoirSpecies = ReservoirSpecies.create_dissolved(
-    "H2O", solubility=solubility_models["H2O_peridotite_sossi23"]
+    "H2O", solubility=solubility_models["H2O_peridotite_sossi23"], include_in_phase_mass=False
 )
 melt: MeltPhase = MeltPhase((H2O_di,))
 gas_HO_model: EquilibriumModel = EquilibriumModel(gas, melt=melt)
@@ -60,7 +60,9 @@ def test_H2O() -> None:
 
     model.solve(state=planet, mass_constraints=mass_constraints)
 
-    target: dict[str, Any] = {"gas": {"partial_pressure_bar": {"H2O_g": 1.0312913336898137}}}
+    target: dict[str, Any] = {
+        "gas": {"species": {"partial_pressure_bar": {"H2O_g": 1.0312913336898137}}}
+    }
 
     # output.to_excel("test_H2O")
 
@@ -83,10 +85,12 @@ def test_H_O() -> None:
 
     fastchem_result: dict[str, Any] = {
         "gas": {
-            "partial_pressure_bar": {
-                "H2O_g": 76.45861543,
-                "H2_g": 73.84378192,
-                "O2_g": 8.91399329e-08,
+            "species": {
+                "partial_pressure_bar": {
+                    "H2O_g": 76.45861543,
+                    "H2_g": 73.84378192,
+                    "O2_g": 8.91399329e-08,
+                }
             }
         }
     }
@@ -111,10 +115,12 @@ def test_H_fO2() -> None:
 
     target: dict[str, Any] = {
         "gas": {
-            "partial_pressure_bar": {
-                "H2O_g": 0.2570800742364775,
-                "H2_g": 0.2491511264610601,
-                "O2_g": 8.838513516896038e-08,
+            "species": {
+                "partial_pressure_bar": {
+                    "H2O_g": 0.2570800742364775,
+                    "H2_g": 0.2491511264610601,
+                    "O2_g": 8.838513516896038e-08,
+                }
             }
         }
     }
@@ -137,21 +143,60 @@ def test_H_fO2_fH2() -> None:
 
     target: dict[str, Any] = {
         "gas": {
-            "partial_pressure_bar": {
-                "H2O_g": np.array(
-                    [3.262913506271090e-09, 1.031823848794260e-07, 3.262913506271089e-06]
-                ),
-                "H2_g": np.array(
-                    [1.000000000000005e-08, 9.999999999999959e-08, 1.000000000000000e-06]
-                ),
-                "O2_g": np.array(
-                    [8.838513516896060e-09, 8.838513516896038e-08, 8.838513516896018e-07]
-                ),
+            "species": {
+                "partial_pressure_bar": {
+                    "H2O_g": np.array(
+                        [3.262913506271090e-09, 1.031823848794260e-07, 3.262913506271089e-06]
+                    ),
+                    "H2_g": np.array(
+                        [1.000000000000005e-08, 9.999999999999959e-08, 1.000000000000000e-06]
+                    ),
+                    "O2_g": np.array(
+                        [8.838513516896060e-09, 8.838513516896038e-08, 8.838513516896018e-07]
+                    ),
+                }
             }
         }
     }
 
     assert gas_HO_model.output.compare(target, rtol=RTOL, atol=ATOL)
+
+
+def test_H_fO2_fH2_new() -> None:
+    """Tests H2-H2O at the IW buffer with H2O solubility and mixed fugacity constraints."""
+
+    planet: Planet = Planet()
+    fugacity_constraints: dict[str, FugacityConstraintProtocol] = {
+        "H2_g": FixedFugacityConstraint(np.array([1.0e-8, 1.0e-7, 1.0e-6])),
+        "O2_g": IronWustiteBuffer(),  # np.array([-1, 0, 1])),
+    }
+    mass_constraints: dict[str, ArrayLike] = {"H": earth_oceans_to_hydrogen_mass(1)}
+
+    gas_HO_model.solve(
+        state=planet, fugacity_constraints=fugacity_constraints, mass_constraints=mass_constraints
+    )
+
+    print(gas_HO_model.output.quick_look())
+
+    # target: dict[str, Any] = {
+    #     "gas": {
+    #         "species": {
+    #             "partial_pressure_bar": {
+    #                 "H2O_g": np.array(
+    #                     [3.262913506271090e-09, 1.031823848794260e-07, 3.262913506271089e-06]
+    #                 ),
+    #                 "H2_g": np.array(
+    #                     [1.000000000000005e-08, 9.999999999999959e-08, 1.000000000000000e-06]
+    #                 ),
+    #                 "O2_g": np.array(
+    #                     [8.838513516896060e-09, 8.838513516896038e-08, 8.838513516896018e-07]
+    #                 ),
+    #             }
+    #         }
+    #     }
+    # }
+
+    # assert gas_HO_model.output.compare(target, rtol=RTOL, atol=ATOL)
 
 
 def test_H_fO2_batch_temperature() -> None:
@@ -174,31 +219,33 @@ def test_H_fO2_batch_temperature() -> None:
 
     target: dict[str, Any] = {
         "gas": {
-            "partial_pressure_bar": {
-                "H2O_g": np.array(
-                    [
-                        2.566653037020448e-01,
-                        2.570800742364757e-01,
-                        2.572178041535549e-01,
-                        2.572746043480848e-01,
-                    ]
-                ),
-                "H2_g": np.array(
-                    [
-                        3.133632393608037e-01,
-                        2.491511264610584e-01,
-                        2.265704456625875e-01,
-                        2.199521409043987e-01,
-                    ]
-                ),
-                "O2_g": np.array(
-                    [
-                        2.394194493859141e-12,
-                        8.838513516896038e-08,
-                        4.544970468047975e-05,
-                        2.739422634823809e-03,
-                    ]
-                ),
+            "species": {
+                "partial_pressure_bar": {
+                    "H2O_g": np.array(
+                        [
+                            2.566653037020448e-01,
+                            2.570800742364757e-01,
+                            2.572178041535549e-01,
+                            2.572746043480848e-01,
+                        ]
+                    ),
+                    "H2_g": np.array(
+                        [
+                            3.133632393608037e-01,
+                            2.491511264610584e-01,
+                            2.265704456625875e-01,
+                            2.199521409043987e-01,
+                        ]
+                    ),
+                    "O2_g": np.array(
+                        [
+                            2.394194493859141e-12,
+                            8.838513516896038e-08,
+                            4.544970468047975e-05,
+                            2.739422634823809e-03,
+                        ]
+                    ),
+                }
             }
         }
     }
@@ -228,31 +275,33 @@ def test_H_fO2_batch_fO2_shift() -> None:
 
     target: dict[str, Any] = {
         "gas": {
-            "partial_pressure_bar": {
-                "H2O_g": np.array(
-                    [
-                        2.974916728388850e-04,
-                        1.609262626310640e-01,
-                        2.583020136413676e-01,
-                        2.585402668639946e-01,
-                    ]
-                ),
-                "H2_g": np.array(
-                    [
-                        2.883163373152490e01,
-                        7.239157580397396e00,
-                        5.393312233908341e-03,
-                        2.505662833497315e-06,
-                    ]
-                ),
-                "O2_g": np.array(
-                    [
-                        8.838513516896005e-18,
-                        4.102474564576031e-11,
-                        1.904200012911665e-04,
-                        8.838513516896137e02,
-                    ]
-                ),
+            "species": {
+                "partial_pressure_bar": {
+                    "H2O_g": np.array(
+                        [
+                            2.974916728388850e-04,
+                            1.609262626310640e-01,
+                            2.583020136413676e-01,
+                            2.585402668639946e-01,
+                        ]
+                    ),
+                    "H2_g": np.array(
+                        [
+                            2.883163373152490e01,
+                            7.239157580397396e00,
+                            5.393312233908341e-03,
+                            2.505662833497315e-06,
+                        ]
+                    ),
+                    "O2_g": np.array(
+                        [
+                            8.838513516896005e-18,
+                            4.102474564576031e-11,
+                            1.904200012911665e-04,
+                            8.838513516896137e02,
+                        ]
+                    ),
+                }
             }
         }
     }
@@ -280,16 +329,18 @@ def test_H_fO2_batch_H_mass() -> None:
 
     target: dict[str, Any] = {
         "gas": {
-            "partial_pressure_bar": {
-                "H2O_g": np.array(
-                    [2.570800742364757e-01, 2.426110356931991e01, 1.610286613431932e03]
-                ),
-                "H2_g": np.array(
-                    [2.491511264610584e-01, 2.351283467393216e01, 1.560621626756960e03]
-                ),
-                "O2_g": np.array(
-                    [8.838513516896038e-08, 8.838513516896038e-08, 8.838513516896102e-08]
-                ),
+            "species": {
+                "partial_pressure_bar": {
+                    "H2O_g": np.array(
+                        [2.570800742364757e-01, 2.426110356931991e01, 1.610286613431932e03]
+                    ),
+                    "H2_g": np.array(
+                        [2.491511264610584e-01, 2.351283467393216e01, 1.560621626756960e03]
+                    ),
+                    "O2_g": np.array(
+                        [8.838513516896038e-08, 8.838513516896038e-08, 8.838513516896102e-08]
+                    ),
+                }
             }
         }
     }
